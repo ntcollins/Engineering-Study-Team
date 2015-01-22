433e055811acda7522767d4cbec9e4f9f8eb1e04
ntcollins <tcollins@shampooshampoo.com>
2015-01-22 15:02:23 -0800

    Fix $count_k math

--- Filter_H.pl
+++ Filter_H.pl
@@ -1,4 +1,4 @@
-printf("%f\n", filter_H(1, 1, 15));
+printf("%f\n", filter_H(1, 1, 15) );
 
 sub filter_H
 {
@@ -6,7 +6,7 @@ sub filter_H
  my $k_sum = 0.0;
  for(my $count_k = 0; $count_k <= $k; $count_k++)
  {
-	$k_sum += $x * ($i-$k);
+	$k_sum += $x * ($i-$count_k);
  }
  return ((1.0/16.0) * $k_sum);
 }
