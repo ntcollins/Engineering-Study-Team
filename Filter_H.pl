849835b1e182ba07742ba1bc8b0e67f2657d6730
ntcollins <tcollins@shampooshampoo.com>
2015-01-22 00:08:31 -0800

    Filter_H

---
+++ Filter_H.pl
@@ -0,0 +1,12 @@
+printf("%f\n", filter_H(1, 1, 15));
+
+sub filter_H
+{
+ my ($x, $i, $k) = @_;
+ my $k_sum = 0.0;
+ for(my $count_k = 0; $count_k <= $k; $count_k++)
+ {
+	$k_sum += $x * ($i-$k);
+ }
+ return ((1.0/16.0) * $k_sum);
+}
